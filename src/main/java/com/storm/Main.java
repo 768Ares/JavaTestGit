70dd003b0ef3e7e258b05339cfd7770e9c315f29
Daniel <danieljaskolski768@gmail.com>
2018-08-20 19:35:49 +0200

    master test

--- src/main/java/com/storm/Main.java
+++ src/main/java/com/storm/Main.java
@@ -9,6 +9,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
+        System.out.println("master test");
     }
 
     @Override
